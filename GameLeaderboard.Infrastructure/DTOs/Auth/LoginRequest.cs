023574bd3882f7be8844ed90277626abc1a8366e
Simone Sommaggio <simosommaggio@gmail.com>
2026-05-07 11:33:11 +0200

    Remove data annotations

--- GameLeaderboard.Infrastructure/DTOs/Auth/LoginRequest.cs
+++ GameLeaderboard.Infrastructure/DTOs/Auth/LoginRequest.cs
@@ -1,8 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace GameLeaderboard.Infrastructure.DTOs.Auth;
 
 public record LoginRequest(
-    [Required] [StringLength(50)] [EmailAddress] string Email,
-    [Required] [StringLength(50)] string Password
+    string Email,
+    string Password
 );
--- GameLeaderboard.Infrastructure/DTOs/Leaderboard/SubmitScoreRequest.cs
+++ GameLeaderboard.Infrastructure/DTOs/Leaderboard/SubmitScoreRequest.cs
@@ -1,8 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace GameLeaderboard.Infrastructure.DTOs.Leaderboard;
 
 public record SubmitScoreRequest(
-    [Required] [Range(0, int.MaxValue)] int Amount,
-    [Required] DateTime CreatedAt
+    int Amount,
+    DateTime CreatedAt
 );
