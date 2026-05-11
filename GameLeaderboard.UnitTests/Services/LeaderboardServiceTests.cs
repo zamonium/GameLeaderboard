3707b934a8abb789d6e3ebe0d91405208b0a937b
Simone Sommaggio <simosommaggio@gmail.com>
2026-05-11 12:28:28 +0200

    Improved unit tests

--- GameLeaderboard.UnitTests/Services/LeaderboardServiceTests.cs
+++ GameLeaderboard.UnitTests/Services/LeaderboardServiceTests.cs
@@ -48,16 +48,30 @@ public class LeaderboardServiceTests
         db.Scores.Should().ContainSingle(s => s.Amount == 100 && s.UserId == 1);
     }
 
-    [Fact]
-    public async Task SubmitScore_should_throw_when_userId_is_invalid()
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public async Task SubmitScore_should_throw_when_userId_is_invalid(int userId)
     {
         await using var db = TestDbContextFactory.Create();
         var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
 
         var request = new SubmitScoreRequest(Amount: 100, CreatedAt: DateTime.UtcNow);
-        var act = () => service.SubmitScore(userId: -1, request, CancellationToken.None);
+        var act = () => service.SubmitScore(userId: userId, request, CancellationToken.None);
 
         await act.Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Fact]
+    public async Task GetAllScoresAsync_should_return_empty_list_when_database_is_empty()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var result = await service.GetAllScoresAsync(new GetScoresRequest(), CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.TotalCount.Should().Be(0);
     }
 
     [Fact]
@@ -80,5 +94,117 @@ public class LeaderboardServiceTests
         result.Success.Should().BeTrue();
         result.Data!.Items.Select(i => i.Amount).Should().ContainInOrder(30, 20, 10);
         result.Data.TotalCount.Should().Be(3);
+    }
+
+    [Fact]
+    public async Task GetAllScoresAsync_should_return_paged_score_sorted_by_created_at_desc()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var user = new User {Email = "test@example.com", Username = "test"};
+        db.Users.Add(user);
+        db.Scores.AddRange(
+            new Score {Amount = 10, CreatedAt = new DateTime(2026, 1, 1, 12, 0, 0), User = user},    
+            new Score {Amount = 20, CreatedAt = new DateTime(2026, 1, 2, 12, 0, 0), User = user},    
+            new Score {Amount = 30, CreatedAt = new DateTime(2026, 1, 3, 12, 0, 0), User = user}    
+        );
+        await db.SaveChangesAsync();
+
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var request = new GetScoresRequest { SortBy = "CreatedAt", Desc = true};
+        var result = await service.GetAllScoresAsync(request, CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.Items.Select(i => i.Amount).Should().ContainInOrder(30, 20, 10);
+        result.Data.TotalCount.Should().Be(3);
+    }
+
+    [Fact]
+    public async Task GetAllScoresAsync_should_return_paged_score_when_pagination_parameter_specified()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var user = new User {Email = "test@example.com", Username = "test"};
+        db.Users.Add(user);
+        db.Scores.AddRange(
+            new Score {Amount = 10, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 20, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 30, CreatedAt = DateTime.UtcNow, User = user}
+        );
+        await db.SaveChangesAsync();
+
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var request = new GetScoresRequest { Page = 2, PageSize = 2};
+        var result = await service.GetAllScoresAsync(request, CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.Items.Select(i => i.Amount).Should().ContainInOrder(10);
+        result.Data.TotalCount.Should().Be(3);
+        result.Data.TotalPages.Should().Be(2);
+    }
+
+    [Fact]
+    public async Task GetUserScoresAsync_should_return_user_score()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var user = new User {Email = "test@example.com", Username = "test"};
+        db.Users.Add(user);
+        db.Scores.AddRange(
+            new Score {Amount = 10, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 20, CreatedAt = DateTime.UtcNow, User = user}
+        );
+        await db.SaveChangesAsync();
+
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var result = await service.GetUserScoresAsync(user.Username, new GetScoresRequest(), CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.Items.Select(i => i.Amount).Should().ContainInOrder(20, 10);
+        result.Data.TotalCount.Should().Be(2);
+    }
+
+    [Fact]
+    public async Task GetUserScoresAsync_should_return_only_user_score()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var user = new User {Email = "test@example.com", Username = "test"};
+        db.Users.Add(user);
+        var user2 = new User {Email = "test2@example.com", Username = "test2"};
+        db.Users.Add(user2);
+        db.Scores.AddRange(
+            new Score {Amount = 10, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 20, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 30, CreatedAt = DateTime.UtcNow, User = user2}
+        );
+        await db.SaveChangesAsync();
+
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var result = await service.GetUserScoresAsync(user.Username, new GetScoresRequest(), CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.Items.Select(i => i.Amount).Should().ContainInOrder(20, 10);
+        result.Data.TotalCount.Should().Be(2);
+    }
+
+    [Fact]
+    public async Task GetUserScoresAsync_should_return_empty_list_when_user_has_no_score()
+    {
+        await using var db = TestDbContextFactory.Create();
+        var user = new User {Email = "test@example.com", Username = "test"};
+        var user2 = new User {Email = "test2@example.com", Username = "test2"};
+        db.Scores.AddRange(
+            new Score {Amount = 10, CreatedAt = DateTime.UtcNow, User = user},
+            new Score {Amount = 20, CreatedAt = DateTime.UtcNow, User = user}
+        );
+        await db.SaveChangesAsync();
+
+        var service = new LeaderboardService(db, NullLogger<LeaderboardService>.Instance);
+
+        var result = await service.GetUserScoresAsync(user2.Username, new GetScoresRequest(), CancellationToken.None);
+
+        result.Success.Should().BeTrue();
+        result.Data!.TotalCount.Should().Be(0);
     }
 }
